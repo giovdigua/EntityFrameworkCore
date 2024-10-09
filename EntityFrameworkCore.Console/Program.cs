37fbe8a756ab4c46bd4c913204383dfc1bb39342
gdiguardo-eprj <gdiguardo@e-project.it>
2024-10-09 13:42:14 +0200

    Add project files.

---
+++ EntityFrameworkCore.Console/Program.cs
@@ -0,0 +1,39 @@
+﻿//First we need an instance of context
+using EntityFrameworkCore.Data;
+using Microsoft.EntityFrameworkCore;
+
+using var context = new FootballLeagueDbContext();
+
+// Select all teams
+//GetAllTeams();
+
+// Selecting a single record - First one in list
+//var teamOne = await context.Coaches.FirstOrDefaultAsync();
+//var teamOne = await context.Teams.FirstOrDefaultAsync();
+
+// Selecting a single record - First one in list that meets a condition
+//var teamTwo = await context.Teams.FirstAsync(team => team.TeamId == 1);
+//var teamTwo = await context.Teams.FirstOrDefaultAsync(team => team.TeamId == 1);
+
+// Selecting a single record - Only one record should be returned
+//var teamsThree = await context.Teams.SingleAsync();
+//var teamThree = await context.Teams.SingleAsync(team => team.TeamId == 2);
+//var teamFour = await context.Teams.SingleOrDefaultAsync(team => team.TeamId == 2);
+
+// selecting based on Id
+var teamBasedOnId = await context.Teams.FindAsync(3);
+if (teamBasedOnId != null )
+{
+    Console.WriteLine(teamBasedOnId.Name);
+}
+void GetAllTeams()
+{
+    // SELECT * FROM teams
+    var teams = context.Teams.ToList();
+
+    foreach (var t in teams)
+    {
+        Console.WriteLine(t.Name);
+    }
+}
+
---
+++ EntityFrameworkCore.Data/FootballLeagueDbContext.cs
@@ -0,0 +1,57 @@
+﻿using EntityFrameworkCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.Data
+{
+    public class FootballLeagueDbContext : DbContext
+    {
+        public FootballLeagueDbContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DBPath = Path.Combine(path, "FootbalLeague_EFCore.db");
+        }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Coach> Coaches { get; set; }
+        public string DBPath { get; private set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=FootbalLeague_EFCore; Encrypt=False");
+            optionsBuilder.UseSqlite($"Data Source={DBPath}")
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                //Not in productions
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Team>().HasData(
+                    new Team
+                    {
+                        TeamId = 1,
+                        Name = "Tivoli Gardens FC",
+                        CreatedDate = DateTimeOffset.UtcNow.DateTime,
+                    },
+                    new Team
+                    {
+                        TeamId = 2,
+                        Name = "Waterhouse F.C.",
+                        CreatedDate = DateTimeOffset.UtcNow.DateTime,
+                    }, new Team
+                    {
+                        TeamId = 3,
+                        Name = "Humble Lions F.C.",
+                        CreatedDate = DateTimeOffset.UtcNow.DateTime,
+                    }
+                );
+        }
+    }
+}
